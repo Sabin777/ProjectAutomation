0ae8dbae45cd005ee6afe339b11a23286a310da6
Sabin Karki <sabinkarki@Sabins-MacBook-Pro.local>
2022-02-20 16:27:20 -0700

    test case added

--- Automation-test/WebBrowser-Tests.cs
+++ Automation-test/WebBrowser-Tests.cs
@@ -1,10 +1,41 @@
 ﻿using System;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
 namespace Automation_test
 {
     public class WebBrowser_Tests
     {
-        public WebBrowser_Tests()
+        public IWebDriver driver;
+
+        [SetUp]
+        public void Setup()
         {
+            driver = new ChromeDriver();
+        }
+
+        [Test]
+        public void VerifyingWebPage()
+        {
+            driver.Navigate().GoToUrl("https://www.healthgrades.com/physician/dr-sandy-test-provider-xyp88j3?referrerSource=autosuggest");
+
+            driver.Quit();
+
+        }
+
+        [Test]
+        public void VerifyingReviewTitle()
+        {
+            driver.Navigate().GoToUrl("https://www.healthgrades.com/physician/dr-sandy-test-provider-xyp88j3?referrerSource=autosuggest");
+
+            var findElements = By.XPath("//h2[@class='title--accented']");
+            var element = driver.FindElement(findElements).Text;
+
+
+            Assert.AreEqual("Dr. Test Provider's Reviews", element);
+            driver.Quit();
+
         }
     }
 }
