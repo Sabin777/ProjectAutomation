ec05846475312616f77e4225d875920d4bfa5960
Sabin Karki <sabinkarki@Sabins-MacBook-Pro.local>
2022-02-20 15:30:53 -0700

    initial commit

---
+++ Automation-test/UnitTest1.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+
+namespace Automation_test
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
---
+++ AutomationService/Calculator.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace AutomationService
+{
+    public class Calculator
+    {
+       public decimal Add(decimal firstDecimal, decimal secondDecimal)
+        {
+            decimal result;
+            result = firstDecimal + secondDecimal;
+            return result; 
+        } 
+    }
+}
